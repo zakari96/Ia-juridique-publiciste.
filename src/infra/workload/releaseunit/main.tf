9e4ec894828a570a7a4c96ae04dae2fe506e6a02
zakari96 <158622814+zakari96@users.noreply.github.com>
2025-01-14 20:59:52 +0000

    Initialize no-diag

---
+++ src/infra/workload/releaseunit/main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.71.0"
+    }
+    azapi = {
+      source  = "Azure/azapi"
+      version = "1.9.0"
+    }
+  }
+
+  backend "azurerm" {}
+}
+
+provider "azurerm" {
+  features {
+    resource_group {
+      # Allows the deletion of non-empty resource groups
+      # This is required to delete rgs with stale resources left
+      prevent_deletion_if_contains_resources = false
+    }
+  }
+}
+
+provider "azapi" {}
+
+# Random API key which needs to be identical between all stamps
+resource "random_password" "api_key" {
+  length  = 32
+  special = false
+}
